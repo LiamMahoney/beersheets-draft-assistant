9c04ab51cdc17819c1ff0f8a849d904fb6b1b0d4
LiamMahoney <liammahoney96@gmail.com>
2021-09-06 09:46:38 -0500

    proper default value syntax

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -61,13 +61,14 @@ const LandingPage = (props) => {
                 <Select
                     onChange={handleNoTeamChange}
                     width="80px"
+                    default='10'
                 >
                     <option value='5'>5</option>
                     <option value='6'>6</option>
                     <option value='7'>7</option>
                     <option value='8'>8</option>
                     <option value='9'>9</option>
-                    <option value='10' selected={true}>10</option>
+                    <option value='10'>10</option>
                     <option value='11'>11</option>
                     <option value='12'>12</option>
                     <option value='13'>13</option>
