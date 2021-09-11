8a100a6f9562e3d3e218dba75f903c3911bea35f
LiamMahoney <liammahoney96@gmail.com>
2021-09-11 10:41:26 -0500

    displays round number at start of round

--- src/components/DraftedPlayerTicker.js
+++ src/components/DraftedPlayerTicker.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import DraftTickerPlayer from './DraftTickerPlayer';
 
 const DraftedPlayerTicker = ({ draftedPlayers, numberOfTeams }) => {
 
     const sortedPlayers = draftedPlayers.sort((a, b) => b.pick - a.pick)
+
+    let objects = [];
+
+    // allows us to create a break between rounds within the ticker
+    sortedPlayers.forEach((p) => {
+        const round = p.pick % numberOfTeams === 0 ? p.pick / numberOfTeams : Math.floor(p.pick / numberOfTeams) + 1;
+        const roundPick = p.pick % numberOfTeams === 0 ? numberOfTeams : p.pick % numberOfTeams;
+
+        objects.push(<DraftTickerPlayer
+            key={p.pick}
+            position={p.Pos}
+            name={p.Name}
+            team={p['Tm/Bye']}
+            pick={p.pick}
+            round={round}
+            roundPick={roundPick}
+        />)
+
+        if (roundPick === 1) {
+            // first pick of round - want to show the round in the ticker
+            objects.push(
+                <Text
+                    style={{'writing-mode': 'vertical-rl'}}
+                >
+                    Round {round}
+                </Text>
+            )
+        }
+    });
 
     return (
         <Flex 
@@ -16,19 +45,7 @@ const DraftedPlayerTicker = ({ draftedPlayers, numberOfTeams }) => {
             flexWrap="wrap" 
             alignContent="flex-start"
         >
-            {sortedPlayers.map((p) => {
-                return (
-                    <DraftTickerPlayer
-                        key={p.pick}
-                        position={p.Pos}
-                        name={p.Name}
-                        team={p['Tm/Bye']}
-                        pick={p.pick}
-                        round={p.pick % numberOfTeams === 0 ? p.pick / numberOfTeams : Math.floor(p.pick / numberOfTeams) + 1}
-                        roundPick={p.pick % numberOfTeams === 0 ? numberOfTeams : p.pick % numberOfTeams}
-                    />
-                )
-            })}
+            {objects}
         </Flex>
     )
 }
