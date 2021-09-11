079564a28d924aa616523e11fb37f137b9eed91e
LiamMahoney <liammahoney96@gmail.com>
2021-09-10 23:54:23 -0500

    filling in background instead of just border color

--- src/components/DraftTickerPlayer.js
+++ src/components/DraftTickerPlayer.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, useColorModeValue } from '@chakra-ui/react';
 
 const DraftTickerPlayer = (props) => {
 
     const positionColorMap = {
-        qb: 'red.300',
-        rb: 'blue.300',
-        wr: 'green.300',
-        te: 'orange.300',
-        def: 'yellow.300',
-        k: 'purple.300'
+        qb: useColorModeValue('red.300', 'red.600'),
+        rb: useColorModeValue('blue.300', 'blue.600'),
+        wr: useColorModeValue('green.300', 'green.600'),
+        te: useColorModeValue('orange.300', 'orange.600'),
+        def: useColorModeValue('yellow.300', 'yellow.600'),
+        k: useColorModeValue('purple.300', 'purple.600')
     };
 
     return (
         <Flex
-            border="2px"
-            borderColor={positionColorMap[props.position.toLowerCase()]}
-            width="100px"
+            // border="1px"
+            backgroundColor={positionColorMap[props.position.toLowerCase()]}
+            width="110px"
             margin={3}
             height="80%"
-            maxHeight="100px"
+            maxHeight="110px"
             flexDirection="column"
-            padding={0.5}
+            padding={1}
             boxShadow="sm"
+            borderRadius={5}
         >
             <Text fontSize="xs">{props.position} - {props.team}</Text>
             <Flex flexGrow="1" flexDirection="column" justifyContent="flex-start">
